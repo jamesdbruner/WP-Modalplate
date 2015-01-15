56eaa35c17db4c669bfff8c7a30039111253efab
James <jamesdbruner@gmail.com>
2015-01-14 17:40:55 -0700

    initial commit
    version 1.0

---
+++ wp-modalplate/methods/trigger.php
@@ -0,0 +1,12 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+    $trigger .= <<<HEREDOC
+	<script>
+	jQuery(document).ready(function()
+	{	
+ 	   jQuery('.modal-trigger').modalplate();
+	});
+	</script>
+
+HEREDOC;
